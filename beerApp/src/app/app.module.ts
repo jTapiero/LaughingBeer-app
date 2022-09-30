eeb8d87bc1d425bed5a37e689858b0c53c0d3b74
yossef hillel tapiero <yossefhilleltapiero@hotmail.fr>
2022-09-30 16:30:17 +0300

    app header
    step 1

--- beerApp/src/app/app.module.ts
+++ beerApp/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { FavouriteBeerPageComponent } from './page/favourite-beer-page/favourite-beer-page.component';
 import { SearchBeerPageComponent } from '@page/search-beer-page/search-beer-page.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './component/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     FavouriteBeerPageComponent,
     SearchBeerPageComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
